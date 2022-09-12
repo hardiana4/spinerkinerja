62866c23c6538ac158b0c286e19fe3eeb07908ac
Hardiana <87222054+hardiana4@users.noreply.github.com>
2022-09-12 08:53:44 +0700

    Update p_kinerja.blade.php

--- views/pegawai/p_kinerja.blade.php
+++ views/pegawai/p_kinerja.blade.php
@@ -8,7 +8,7 @@
         <div class="row mb-4">
             <h1 class="md-0">Data Kinerja Pegawai</h1>
             <div class="col box-header text-right">
-                <a href="{{url('/pegawai/tambah-kinerja-pegawai')}}" class="btn btn-primary"><i class="fa fa-plus-circle" ></i &nbsp> Tambah Kinerja</a>
+                <a href="{{url('/pegawai/tambah-kinerja-pegawai')}}" class="btn btn-rounded btn-primary" style="border-radius: 30px;"><i class="fa fa-plus-circle" ></i &nbsp> Tambah Kinerja</a>
           </div><!-- /.col -->
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->
@@ -44,9 +44,8 @@
                 <th style="text-align:center">No</th>
                 <th style="text-align:center">Tgl</th>
                 <th style="text-align:center">Hasil</th>
-                <th style="text-align:center">Bukti</th>
-                <th style="text-align:center">Bukti</th>
-                <th style="text-align:center">Status</th>
+                <th style="text-align:center">Foto</th>
+                <th style="text-align:center">PDF</th>
                 <th style="text-align:center">Aksi</th>
             </thead>
             <tbody>
@@ -56,10 +55,8 @@
                 <td class="text-center">{{$no++}}</td>
                 <td class="text-center">{{$k->tgl}}</td>
                 <td class="text-center">{{$k->hasil}}</td>
-                <td class="text-center"><img src="{{asset('template/dist/img/kinerja/'.$k['foto'])}}" height="100"></td>
-                <td class="text-center"><embed src="{{asset('template/dist/img/kinerja/'.$k['doc'])}}" style="width: 50%"></td>
-                <td class="text-center">{{$k->status}}</td>
-                <td>
+                <td class="text-center"><a href="{{ asset('template/dist/img/kinerja/'.$k['foto']) }}" class="btn btn-rounded btn-info" style="border-radius:30px;"><i class="far fa-file-image"></i></a></td>
+                <td class="text-center"><a href="{{ asset('template/dist/img/kinerja/'.$k['doc']) }}" class="btn btn-rounded btn-info" style="border-radius:30px;"><i class="far fa-file-pdf"></i></a></td>                <td>
                 <div class="d-grid gap-2 d-md-block" style="text-align:center">
                   <a href="{{url('/pegawai/edit-kinerja-pegawai')}}" class="btn btn-warning"><i class="fas fa-edit"></i></a>
                   <a href="/pegawai/hapus/{{ $k->id }}" class="btn btn-danger"><i class="fas fa-trash"></i></a>
@@ -69,35 +66,11 @@
             @endforeach
             </tbody>
           </table>
+          <div class="my-2">
+            {{$kinerja->links()}}
+          </div>
         </div>
     </div>
-    <div class="row">
-        <div class="col-sm-9 col-md-9">
-            <div class="dataTables_info" id="example2_info" role="status" aria-live="polite">Showing 1 to 10 of 30 entries
-            </div>
-        </div>
-          <div class="col-sm-3 col-md-3" style="float: right;">
-              <div class="dataTables_paginate paging_simple_numbers" id="example2_paginate">
-                    <ul class="pagination">
-                        <li class="paginate_button page-item previous disabled" id="example2_previous">
-                            <a href="#" aria-controls="example2" data-dt-idx="0" tabindex="0" class="page-link">Previous</a>
-                        </li>
-                        <li class="paginate_button page-item active">
-                            <a href="#" aria-controls="example2" data-dt-idx="1" tabindex="0" class="page-link">1</a>
-                        </li>
-                        <li class="paginate_button page-item ">
-                            <a href="#" aria-controls="example2" data-dt-idx="2" tabindex="0" class="page-link">2</a>
-                        </li>
-                        <li class="paginate_button page-item ">
-                            <a href="#" aria-controls="example2" data-dt-idx="3" tabindex="0" class="page-link">3</a>
-                        </li>
-                        <li class="paginate_button page-item next" id="example2_next">
-                            <a href="#" aria-controls="example2" data-dt-idx="7" tabindex="0" class="page-link">Next</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
       </div>
     </div>
         </div>
